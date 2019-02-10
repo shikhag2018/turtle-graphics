18202e0688d45731496b8d1fe26528965324460b
shikha gupta <40258568+shikhag2018@users.noreply.github.com>
2019-02-10 19:13:40 +0530

    Add files via upload

---
+++ graphic.py
@@ -0,0 +1,19 @@
+import turtle
+def draw_graphics():
+    turtle.bgcolor("black")
+    brad=turtle.Turtle()
+    i=0
+    brad.color("yellow")
+    while(i<4):
+        brad.circle(100)
+        brad.circle(80)
+        brad.circle(60)
+        brad.circle(40)
+        brad.circle(20)
+        brad.left(90)
+        i=i+1
+            
+        
+        
+draw_graphics()
+   
